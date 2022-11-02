7094871b1bb3cfb1ee2b7f00f4da9054c4193496
Javier Figueroa <javierfigueroamieres.1b@gmail.com>
2022-11-02 14:01:22 -0300

    header component added

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home.component';
 
 
 
 @NgModule({
-  declarations: [
-
-  ],
+  declarations: [HomeComponent,HeaderComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
